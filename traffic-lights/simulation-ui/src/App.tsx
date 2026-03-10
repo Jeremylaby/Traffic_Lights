052b65716aee1cb4f78a2167873b1709914c73cd
Jeremy <stanislaw.barycki@gmail.com>
2026-03-11 00:11:14 +0100

    Route changes

--- traffic-lights/simulation-ui/src/App.tsx
+++ traffic-lights/simulation-ui/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
-import {CrossroadPreview, PreviewPage, RoadPreviewPage} from "@/pages";
+import {CrossroadPreview, PreviewPage, RoadPreviewPage, SimulationPage, StrategyPage} from "@/pages";
 
 function App() {
     return (
         <BrowserRouter>
         <Routes>
+            <Route index element={<StrategyPage />} />
+            <Route path={"/simulation"} element={<SimulationPage/>} />
             <Route path="/preview" element={<PreviewPage />} />
             <Route path={"/preview/road"} element={<RoadPreviewPage/>} />
             <Route path={"/preview/crossroad"} element={<CrossroadPreview/>} />
